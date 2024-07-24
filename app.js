ca9e1e3d5eda9fd1e60a33a2ae63008e8065da00
Zen X <159092319+ZenMuhamadX@users.noreply.github.com>
2024-07-24 11:00:00 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,18 +2,13 @@ const express = require("express");
 const app = express();
 const bot = require("./command");
 
-const setWebHook = () =>{
-bot.telegram.setWebhook("https://webhook.zenxyz.online/hook");
-}
-
 app.use(express.json());
 // WEBHOOK
-app.post("/hook", (req, res) => {
+app.post("/", (req, res) => {
    bot.handleUpdate(req.body, res);
 });
 
 app.get("/",(req,res)=>{
-   setWebHook()
    res.status(200).json({
       OK:true,
       message:"Webhook Set",
